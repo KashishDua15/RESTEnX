effd74f0226e9275df4f2597619bb8f7e2bb44f2
Suvamrx <suvamrx@gmail.com>
2023-10-24 18:06:39 +0530

    adding resturant entity

---
+++ RestEnX/src/main/java/com/fil/restaurantentity/Resturant.java
@@ -0,0 +1,60 @@
+package com.fil.restaurantentity;
+
+public class Resturant {
+	
+	private String name;
+	private String area;
+	private int totalSeats;
+	private int availableSeats;
+	private int mealForTwo;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public int getTotalSeats() {
+		return totalSeats;
+	}
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+	public int getMealForTwo() {
+		return mealForTwo;
+	}
+	public void setMealForTwo(int mealForTwo) {
+		this.mealForTwo = mealForTwo;
+	}
+	public Resturant(String name, String area, int totalSeats, int availableSeats, int mealForTwo) {
+		super();
+		this.name = name;
+		this.area = area;
+		this.totalSeats = totalSeats;
+		this.availableSeats = availableSeats;
+		this.mealForTwo = mealForTwo;
+	}
+	public Resturant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Resturant [name=" + name + ", area=" + area + ", totalSeats=" + totalSeats + ", availableSeats="
+				+ availableSeats + ", mealForTwo=" + mealForTwo + "]";
+	}
+	
+	
+
+}
